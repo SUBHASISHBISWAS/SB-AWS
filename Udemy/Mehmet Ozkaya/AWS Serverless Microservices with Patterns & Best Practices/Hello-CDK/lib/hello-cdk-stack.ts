1ee3703198281818945687734d1b62f257826220
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-12-09 20:15:52 +0530

    CDK Application Queue and S3Bucket

--- Udemy/Mehmet Ozkaya/AWS Serverless Microservices with Patterns & Best Practices/Hello-CDK/lib/hello-cdk-stack.ts
+++ Udemy/Mehmet Ozkaya/AWS Serverless Microservices with Patterns & Best Practices/Hello-CDK/lib/hello-cdk-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue=new Queue(this,"HelloCDKQueue",{
+      visibilityTimeout:Duration.seconds(300)
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const newBucket=new Bucket(this,"SBBucket",{
+      versioned:true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects:true
+    })
+    
   }
 }
